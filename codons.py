469b9b5e4e06409e3b96aab1c927c0a74a4b04b1
324232776 <ledoronlevi@gmail.com>
2025-12-03 23:43:11 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,15 @@
-def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+def create_codon_dict(file_p ath):
 
+codon_dict = {} with open(file_path, 'r') as
 
+for line in file:
+
+file:
+
+codon, amino_acid, single_letter, full_name = line.split()
+
+codon_dict[codon] =
+
+single_letter
+
+return codon_dict
